8c42c4550db645e533ee038abe4a26fa0dc71ebf
Shaista azam <msalmaninaaz4904@gmail.com>
2026-09-16 10:11:13 +0530

    Add files via upload

---
+++ lec 29 js foundation/index.js
@@ -0,0 +1,74 @@
+
+//console.log("hii world");
+//console.log(4+5);
+ //console.log(3-2);
+//console.log(8*3);
+
+//age=23;
+//name= "mansha";
+
+//console.log(age);
+//console.log(name);
+//console.log(age,name);
+
+//var age: //declaration --> box created withaout value
+//age = 24 // intialization --> box me value de di
+//var age = 24; //redeclaration
+age = 24 //updation
+//console.log(age)
+
+//let name = "mansha";
+//console.log(name)
+//let name = "azam"; //re-declaration not allowed
+//name= "azam" //updation allowed 
+//console.log(name)
+ 
+   //const aadharcard = 587996534098
+   //console.log(aadharcard)
+   //const aadharcard = 54843960866976 //re-declaration not allowed
+   //aadharcard = 48964680967998 // updation not allowed
+     
+   //data types = 2 1. primitive 2. non-primitive
+   //1. number, string,bolean,null,undefined,bigInt,symbol.
+   //2. object , array, function.
+
+
+   // aadharCardNumaber//camelcase (good practice) conventions
+   // first_name_ (word jodne ke liye {readable ho jata h underscore dene se} called snake_case)
+   // DB_URL // SCREAMING_SNAKE
+    //let let = "mansha" reserved keyword is not allowed
+
+    // let college_name = "london Academy"; //string
+     // let mobileNumber = 5709-0834943; // number
+     //let discount = 659.8; //number
+     //console.log(typeof college_name);
+     // console.log(mobileNumber);
+     // console.log(discount);
+
+      // discount = "hello" // string;
+     // console.log(typeof discount);
+
+   //  const doubleQuote = "hii dostoo"  //string
+   //  const singleQuote = 'i shaista'     //string
+    const templateLiteral = ` hii meery  
+        hello shaista`  //string
+
+        //string
+    // console.log(doubleQuote);
+    //  console.log(singleQuote);
+    //  console.log(templateLiteral);
+      
+    //  const userName = "sakshi"
+    //  const greetingMessage = ` hii, ${userName}`; // templateLiteral (string)
+    //  console.log(greetingMessage);
+
+      //let user;
+      // console.log(user); //undefined
+
+      //null
+   //   let products =null;
+  //    console.log( typeof products); //intentionally
+
+    //  let isVerifiedIn = true
+   //   console.log(typeof isVerifiedIn) //boolean
+
