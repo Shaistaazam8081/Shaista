d443ac23a3663d0a0a387d4610abf18871f2855e
Shaista azam <msalmaninaaz4904@gmail.com>
2026-09-16 10:14:00 +0530

    Add files via upload

---
+++ lecture 31/function.js
@@ -0,0 +1,70 @@
+
+// let alokMarks = 47 + 78 + 57 ;
+// let sakshiMarks = 67 +90 +78;
+
+// console.log("alokMarks" , alokMarks);
+// console.log("sakshiMarks" , sakshiMarks);
+
+//functon declation ;
+
+ function  totalMarks ( studentsName , mathMarks, sceinceMarks, englshMarks){
+         console.log(`${studentsName} totalMarks`, mathMarks + sceinceMarks + englshMarks)
+ }
+        // return mathMarks + sceinceMarks + englshMarks;
+        //   }
+  
+//     function calPercentage( studentsName, mathsMarks, sceiceMarks, englshMarks) {
+//         let total = totalmarks(mathsMarks,sceiceMarks,englshMarks,)
+//       let percentage = (total / 30 ) * 100 ;
+//       console.log(`${studentsName} percentage :,` percentage);
+    
+// }
+        
+ // totalMarks("sakshi", 45,89,70,90)
+// totalMarks( "kesar" , 90, 78, 68, 40)
+// totalMarks( "rubina", 78 , 90, 28, 60)
+// totalMarks( "aiman" , 40, 89, 60 ,90 )
+// totalMarks( "shreya", 46, 80 ,27, 90) 
+
+
+
+// function greetingMsg(userName,greetings = "hii") {
+//     console.log(`${greetings}, ${userName}` )
+// }
+// greetingMsg("priyanshu" , "hii")
+// greetingMsg("gargi" , "hello")
+// greetingMsg("arsalan" ,"assalamualaikum")
+// greetingMsg( "satyam",  "kemcho")
+// greetingMsg("annu" )
+
+//function expression ;
+
+// let add = function (num1,num2){
+//    return num1 + num2
+// }
+// console.log(add(5,2))
+
+  //Arrow function;
+  
+  //syntax 1
+ // let add = num1 => num1 + 4;
+
+  //syntax 2
+ //  let add = (num1 ,num2) => num1 + num2 ;
+
+//syntax 3
+ // let add = (num1 , num2 )  => {
+        ///something
+        //something...
+        //..
+    
+   //     return num1 + num2;
+        
+ // };
+
+ // console.log ( add (2,9));
+
+ 
+ 
+
+ 
---
+++ lecture 31/index.js
@@ -0,0 +1,66 @@
+
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+
+//loops (repeated task me help krta h)
+//1. for loop {pta hota h khn rukna h} 2. while loop {exit nhi pta}, 3. 
+
+// for (  initialization;condition;update) {
+
+// } // initialization= khn se chlega ; kb tk chlega,update
+
+
+
+// let num = 100 
+
+// for (let i = 1; i <= num; i++) {
+//      console.log(i);
+// }
+
+// while(condition) {
+//      console.log("jii")
+// }
+
+// let i = 1;  
+// while (i <= 5) {
+//      console.log(i)
+//      i++;
+// }
+
+//  
+ 
+
+
+
+// for (let i = 1; i <= 5 ; i++) {
+
+//    if (i === 3){
+//       break
+//    }
+//    console.log(i)
+// }
+
+
+// for (let i = 1; i <= 5 ; i++) {
+
+//    if (i === 3 || i === 4){
+//       continue;
+//    }
+//    console.log(i);
+// }   
+
+
+// for (let i = 1 ; i <= 10 ; i++) {
+//    console.log(2 * i)
+// }  
+
+// let num = 35
+// for (let i = 1 ; i <= 10 ; i++) {
+//    console.log(num * i)
+// } 
